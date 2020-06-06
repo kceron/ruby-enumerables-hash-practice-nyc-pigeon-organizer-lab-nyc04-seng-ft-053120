4e82c096035e0754c15458644dfc11e4e6bd6df5
Karem Ceron <karemiceron@gmail.com>
2020-06-06 01:27:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   organized_data = {}
-  data.each do |pigeon_attributes, attributes_hash|
-    if attributes_hash == true 
+  data.each.each do |pigeon_attributes, attributes_hash|
+      if attributes_hash == true 
       attributes_hash.each do |attributes, name|
         #condition
         organized_data << "#{name} !!!"
@@ -11,17 +11,4 @@ def nyc_pigeon_organizer(data)
       end
     end 
     organized_data
-end
-
-
-def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    if person == "Freddy Mercury"
-      contact_details_hash.each do |attribute, data|
-        if attribute == :favorite_ice_cream_flavors
-          data.delete_if {|ice_cream| ice_cream == "strawberry"}
-        end
-      end
-    end
-  end
 end
