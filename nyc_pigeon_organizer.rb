3349f1a98ac9d733880ce8b2b37481060de2d3cc
Karem Ceron <karemiceron@gmail.com>
2020-06-12 00:17:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,4 @@
 def nyc_pigeon_organizer(data)
-  
   final_hash = data.each_with_object({}) do |(key, value), arr|
     
     value.each do |inner_k, names|
@@ -14,4 +13,22 @@ def nyc_pigeon_organizer(data)
       end 
     end 
   end 
+end
+
+def nyc_pigeon_organizer(data)
+  pigeon_list = {}
+  data.each do |key, value|
+    value.each do |info, all_names|
+      all_names.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name][key]
+          pigeon_list[name][key] = []
+        end
+        pigeon_list[name][key].push(info.to_s)
+      end
+    end
+  end
+  pigeon_list
 end
